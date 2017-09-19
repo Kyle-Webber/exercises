7b4d942a37b7b54a35869560f148e9ec394254ce
Kyle Webber <k0766316@student.kirkwood.edu>
2017-09-19 13:17:41 +0000

    Address Program

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -5,20 +5,21 @@
 # and performs no computations.
 # It will display the name and contact information for a teacher.
 
-# display the full name
 
+# display the full name
+print("Peter Griffin")
 
 # display the address
-
+print("31 Spooner St.")
 
 # display the city, state and zip
-
+print("Quahog, Rhode Island; 32167")
 
 # display the phone number
-
+print("555-420-6969")
 
 # display the course area
-
+print("cis-121")
 
 
 
